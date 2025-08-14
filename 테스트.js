68c29a7543154998770a72427066eef326af7d30
25-1722-source <25-1722@swjb.hs.kr>
2025-08-14 18:49:30 +0900

    2025-08-14

---
+++ 테스트.js
@@ -0,0 +1,7 @@
+let bunho01=1; //값을 할당 ,변수의 자료형 숫자형
+       console.log("bunho01:");
+         console.log(typeof bunho01)
+         let bunho02=1; //값을 할당 ,변수의 자료형 숫자형
+       console.log("bunho02:");
+         console.log(typeof bunho02)
+         function show
